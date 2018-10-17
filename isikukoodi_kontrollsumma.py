ae4572a7494b6fe87e2b8c5913b78f52cc3a8c55
mikkva <mikk.vachtmeister@gmail.com>
2018-10-17 16:59:52 +0300

    Update isikukoodi_kontrollsumma.py
    viimane töötav versioon

--- isikukoodi_kontrollsumma.py
+++ isikukoodi_kontrollsumma.py
@@ -1,27 +1,23 @@
-isikukood = "39506250243"
+isikukood = "39506250234" # vale kood nt 39506250234
 
 def kontrollsumma(isikukood):
     """tagastab isikukoodi kontrollsumma)"""
     
     ikood=list(isikukood)
     sum1=0
-    sum2=0
 
     for i in range (0,9):
        sum1= sum1+ (int(ikood[i])*(i+1))
     sum1 = sum1 + int(ikood[9]) # viimane kaal on 1 sp liidab siin
-    print(sum1)
     kontrollnr= sum1 % 11
-    print(kontrollnr)
 
-    
+
+    sum1=0
     if kontrollnr == 10:
         for i in range (0,7):
-           sum2 = sum2 + (int(ikood[i])*(i+3))
-           print(sum2)
-        sum2 = sum2 + int(ikood[7]) + ((int(ikood[8])*2)) + ((int(ikood[9])*3))
-        kontrollnr=sum2 % (11)
-        print(kontrollnr)
+           sum1 = sum1 + (int(ikood[i])*(i+3))
+        sum1 = sum1 + int(ikood[7]) + ((int(ikood[8])*2)) + ((int(ikood[9])*3))
+        kontrollnr=sum1 % (11)
         if kontrollnr == 10:
             kontrollnr == 0
     
@@ -31,8 +27,8 @@ def kontrollsumma(isikukood):
     else:
         kontroll = 1
     
-    print(kontrollnr)
+    
     return(kontroll)
-print("hei")
+
 
 print(kontrollsumma(isikukood))
